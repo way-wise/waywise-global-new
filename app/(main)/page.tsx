4b3365b225a0cc88133bb49f8820b53a3830d3eb
tauhid97k <tauhid.coder@gmail.com>
2025-10-29 23:50:28 +0600

    fix

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBusiness from "./_components/explore-business";
 import Hero from "./_components/hero";
 import CTA from "./_components/home/cta";
 import Feature from "./_components/home/feature";
@@ -6,6 +7,7 @@ const HomePage = () => {
   return (
     <div>
       <Hero />
+      <ExploreBusiness />
       <Feature />
       <CTA />
     </div>
