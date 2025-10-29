274760db475575cabd9ce3a57efa26f82fc44913
rakib0101 <rakib.01jan@gmail.com>
2025-10-29 23:40:38 +0600

    wip

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "./_components/hero";
+import Feature from "./_components/home/feature";
+import CTA from "./_components/home/cta";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <Feature />
+      <CTA />
     </div>
   );
 };
